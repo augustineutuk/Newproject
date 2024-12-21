ab42111318a687969b0dfbe0533555c1ededd38e
unknown <powerful0148@gmail.com>
2024-12-21 10:39:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const firstElement =document.getElementsByTagName("li")[3];
+firstElement.style.color = 'red';
+firstElement.style.marginLeft = '3rem'
+
+
+
+
+
+const doc = document.querySelector('.hero h2')
+doc.style.color = ' blue';
+
+const doc1 = document.querySelector('.hero2')
+doc1.style.color =  'blue';
+doc1.style.marginLeft = '2rem';
